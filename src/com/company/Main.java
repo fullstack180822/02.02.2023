432e54dfb8ff7062f67afdfbeefaa8eab1bba6d8
micserv010821 <dotnet290320@gmail.com>
2023-02-02 18:11:27 +0200

    upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,35 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static int add_int(int x, int y) {
+        return x + y;
     }
+
+    public static double add_double(double x, double y) {
+        return x + y;
+    }
+
+    // overloading
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static double add(double x, double y) {
+        return x + y;
+    }
+    public static int add(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    // cannot overload with ONLY return type difference
+    //public static int add(double x, double y) {
+    //    return (int)Math.floor(x + y);
+    //}
+
+    public static void main(String[] args) {
+
+        System.out.printf("3 + 4 = %d \n", add(3, 4));
+        System.out.printf("3.5 + 4.2228 = %f", add(3, 4.2228));
+    }
+
+
 }
