0c82de3ff90a6759c15a646cc652601a41700c09
micserv010821 <dotnet290320@gmail.com>
2023-02-02 20:00:05 +0200

    upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,23 @@ package com.company;
 
 public class Main {
 
+     static int z;
+
+    public static int factioral_recursion(int index) {
+        int x = index * 2;
+        if (index <= 1) {
+            return 1;
+        }
+        int z = 3;
+        z = z + 1;
+        System.out.println(z);
+        return index * factioral_recursion((index - 1));
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        int z = 20;
+        int index = 200;
+        int x = 100;
+        System.out.println(factioral_recursion(5));
     }
 }
