ad6215c1e52949e5a8bd4fa44c0b79d991eb4bcd
micserv010821 <dotnet290320@gmail.com>
2023-02-02 21:03:12 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String fname = "Yaniv";
+        String lname = "Cohen";
+
+        String full_name = String.format("{%s}.{%s}", fname, lname);
+
+        String sentence = String.format("Welcome to the course: your first name is {%s} and your last name is: {%s}", fname, lname);
+
+        System.out.printf(sentence);
+
+        int index_of_lname = sentence.indexOf(lname);
+
     }
 }
